4f3c6446a81450591865841f3851618057f589fa
Rohitilak <rohittilak086@gmail.com>
2024-03-26 14:57:43 +0530

    add

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const root = ReactDOM.createRoot(document.getElementById
   ])
 root.render(
   <React.StrictMode>
-  <RouterProvider router={router}></RouterProvider>
+  <RouterProvider router={router} basename='/router'></RouterProvider>
   </React.StrictMode>
 );
 
